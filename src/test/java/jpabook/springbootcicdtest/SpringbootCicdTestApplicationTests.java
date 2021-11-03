6c614181c96fee68c599079c10cd1baee5ad458c
kdk <dogyeong1127@naver.com>
2021-11-03 15:32:07 +0900

    test remove

--- src/test/java/jpabook/springbootcicdtest/SpringbootCicdTestApplicationTests.java
+++ src/test/java/jpabook/springbootcicdtest/SpringbootCicdTestApplicationTests.java
@@ -3,10 +3,10 @@ package jpabook.springbootcicdtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootCicdTestApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
