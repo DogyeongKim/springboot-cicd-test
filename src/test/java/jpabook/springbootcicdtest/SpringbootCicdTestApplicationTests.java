8f3eb375fc9bf89a3766c647408cf8a97ebef6ef
kdk <dogyeong1127@naver.com>
2021-11-09 16:25:44 +0900

    github jenkins test

--- src/test/java/jpabook/springbootcicdtest/SpringbootCicdTestApplicationTests.java
+++ src/test/java/jpabook/springbootcicdtest/SpringbootCicdTestApplicationTests.java
@@ -1,9 +1,11 @@
 package jpabook.springbootcicdtest;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
+@Ignore
 class SpringbootCicdTestApplicationTests {
 
 //	@Test
